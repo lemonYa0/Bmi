1ea3bffe26dc293791b756442ea297497741d725
Sara Lin <lemonsara111@gmail.com>
2023-05-24 23:17:56 +0800

    onclick

--- app/src/main/java/com/tom/bmi/MainActivity.kt
+++ app/src/main/java/com/tom/bmi/MainActivity.kt
@@ -2,11 +2,24 @@ package com.tom.bmi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import com.tom.bmi.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        println("test")
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.edWeight
+        binding.edHeight
+    }
+    fun bmi(view:View){
+        val weight = ed_weight.text.toString().toFloat()
+        val height = ed_height.text.toString().toFloat()
+        val bmi = weight/((height/100)*(height/100))
+        Log.d("BMI",bmi.toString())
     }
 }
